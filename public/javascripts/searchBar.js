8ebc2a0e22cf1596b7666cc252265035c270893a
ArnanK <arnank9@gmail.com>
2024-02-28 00:38:55 -0500

    Frontend Fixes + Navbar features
    -Readjusted Landing Page
    -Added mobile responsiveness
    -Added dropdown menu
    - Implemented following in all other pages
    -Created new directory for index.html file called index.css

--- public/javascripts/searchBar.js
+++ public/javascripts/searchBar.js
@@ -2,12 +2,36 @@ const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 const form = document.getElementById('searchForm');
+const menu = document.querySelector('.menu')
+const menuButton = document.getElementById('menuButton')
+const main = document.querySelector('.main')
 
 
-btn.addEventListener('mouseover', ()=>{
-    if(search.classList.contains('active')){
-        console.log('Search bar is active')
-    }else{
-        search.classList.add('active')
-    }
-})
+function events_Button(){
+    btn.addEventListener('mouseover', ()=>{
+        if(search.classList.contains('active')){
+            console.log('Search bar is active')
+        }else{
+            search.classList.add('active')
+        }
+    })
+
+
+  
+    menuButton.addEventListener('click', ()=>{
+        if(menu.classList.contains('active')){
+            menu.classList.remove('active')
+        }else{
+            menu.classList.add('active')
+        
+        }
+    })
+
+    main.addEventListener('click', ()=>{
+        if(menu.classList.contains('active')){
+            menu.classList.remove('active')
+        }
+    })
+}
+
+events_Button()
