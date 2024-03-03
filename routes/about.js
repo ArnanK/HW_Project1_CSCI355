b6b6f7ae81cf15e55401bbae403e879e1194fb93
Aviv Cohen <avivcohen31@gmail.com>
2024-03-02 22:55:28 -0500

    Add relative path hyperlink and change footer

--- routes/about.js
+++ routes/about.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.use((res, req, next) => {
-    console.log('Time: ', Date.now());
-    next();
-})
-
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'pages', 'about.html'));
 })
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.use((req, res, next) => {
-    console.log('Time: ', Date.now());
-    next();
-})
-
 /* This code snippet is setting up a route using the HTTP GET method for the root URL ("/"). When a GET
 request is made to the root URL, the server will respond by sending the file located at the
 specified path, which is constructed using `path.join(__dirname, '../public', 'pages',
